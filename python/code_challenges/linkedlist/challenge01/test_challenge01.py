74348272a921f242b703a235f5dfac9e07bfe97b
dinashboul <dinaalshboul@gmail.com>
2022-10-25 17:24:43 +0000

    new

--- python/code_challenges/linkedlist/challenge01/test_challenge01.py
+++ python/code_challenges/linkedlist/challenge01/test_challenge01.py
@@ -1,12 +1,12 @@
 # Write your test here
 from re import A
 import pytest 
-from challenge01 import LinkedList,Node,Delete_node
+from challenge01 import LinkedList,Node,delete_node
+
 
 linkedList1 = LinkedList()
 node1 = Node("1")
 linkedList1.append(node1)
-
 node2 = Node("2")
 linkedList1.append(node2)
 
@@ -17,16 +17,15 @@ node4 = Node("4")
 linkedList1.append(node4)
 
 
+def test_delete_node():
+  delete_node(node2)
+  expected = ['1', '3', '4']
+  actual = linkedList1.printAll()
+  assert actual == expected
 
 
-def test_append_node():
-    assert linkedList1.printAll()==['1', '2','3','4']
-
-
-
-
-def test_delete_node ():
-  delete_node1=Delete_node()
-  delete_node1.delete_node(node3)
-
-  assert  linkedList1.printAll() == ['1','2','4']
+def test_delete_tail():
+  linkedList2 = LinkedList()
+  expected ='The linked list is empty'
+  actual = linkedList2.printAll()
+  assert actual ==expected
