47025c8441fd4dc997bdddf411f30cc81f481962
dinashboul <dinaalshboul@gmail.com>
2022-11-05 19:21:10 +0000

    df

--- python/code_challenges/tree/challenge01/challenge01.py
+++ python/code_challenges/tree/challenge01/challenge01.py
@@ -1,38 +1,38 @@
-# Write here the code challenge solution
-from collections import deque
-import queue
+# # Write here the code challenge solution
+# from collections import deque
+# import queue
 
-class Node :
-    def __init__(self,value) :
-        self. value=value
-        self.right=None
-        self.left=None
+# class Node :
+#     def __init__(self,value) :
+#         self. value=value
+#         self.right=None
+#         self.left=None
 
-class Tree:
-    def __init__(self):
-        self.root = None
+# class Tree:
+#     def __init__(self):
+#         self.root = None
 
-class Ordering:
+# class Ordering:
     
-    def BT_traversal(self,preorder,inorder):
+#     def BT_traversal(self,preorder,inorder):
       
-        if not inorder or not preorder:
-            return None
-        if len(inorder)==1 or len(preorder)==1:
-            return preorder[0]
+#         if not inorder or not preorder:
+#             return None
+#         if len(inorder)==1 or len(preorder)==1:
+#             return preorder[0]
         
-        root =Node(preorder[0])
-        mid =inorder.index(preorder[0])
-        root.left= self.BT_traversal(preorder[1:mid+1],inorder[:mid])
-        root.right = self.BT_traversal(preorder[mid+1:],inorder[mid+1:])
-        return root
+#         root =Node(preorder[0])
+#         mid =inorder.index(preorder[0])
+#         root.left= self.BT_traversal(preorder[1:mid+1],inorder[:mid])
+#         root.right = self.BT_traversal(preorder[mid+1:],inorder[mid+1:])
+#         return root
 
 
 
-if __name__=="__main__":
-    preorder=[1,2,3,4,5,6,7]
-    inorder=[3,2,4,1,6,5,7]
-    bt=Ordering()
-    print(bt.BT_traversal(preorder,inorder))
-    # bt.BT_traversal(preorder,inorder)
+# if __name__=="__main__":
+#     preorder=[1,2,3,4,5,6,7]
+#     inorder=[3,2,4,1,6,5,7]
+#     bt=Ordering()
+#     print(bt.BT_traversal(preorder,inorder))
+#     # bt.BT_traversal(preorder,inorder)
 
