9605f4069bd45c0122d20ec8b30e977aa26b82b0
dinashboul <dinaalshboul@gmail.com>
2022-12-17 21:27:11 +0000

    fix hash

--- python/code_challenges/hashtable/challenge01/WhiteBoard.md
+++ python/code_challenges/hashtable/challenge01/WhiteBoard.md
@@ -1,7 +1,7 @@
-![image](./hashtable.png)
+![image](./hash1.png)
 
 1-  how long this assignment took you in the comments box.
-it takes 4 hours
+it takes 5 hours
 
 2- Add any additional comments to your grader about your process or any difficulties you may have had with the assignment. 
 no comments
--- python/code_challenges/hashtable/challenge01/challenge01.py
+++ python/code_challenges/hashtable/challenge01/challenge01.py
@@ -1,35 +1,32 @@
 # Write here the code challenge solution
-
- 
- 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
 class Tree():
     def __init__(self):
-        self.root = None
+       self.root=None 
+
+class Node :
+    def __init__(self,data):
+     self.data = data
+     self.left = None
+     self.right = None
 
 
 
-def findTarget( root, k):
-    '''
-        METHODE to traverce through tree and find two value the summation of these two value equal to given integer
-    '''
-    nums = []
-    inorder(root, nums)
-    for i in range(len(nums)):
-        if k-nums[i] in nums[i+1:]:
+'''
+function that takes a Binary Search tree and an integer as a parameter. 
+Return True if Binary search tree has two elements that their summation is the given integer.    '''
+    
+def tsB(root,k,s):
+   
+    if root:
+        if k - root.data in s:
             return True
-    return False
+        s.add(root.data)
+        return tsB(root.left,k,s) or tsB(root.right,k,s)
+    else:
+        return False
+        
 
-def inorder( node, nums):
-    if node!=None:
-        inorder(node.left, nums)
-        nums.append(node.data)
-        inorder(node.right, nums)
+
 
 if __name__ =='__main__':
     tree=Tree()
@@ -41,12 +38,13 @@ if __name__ =='__main__':
     tree.root.left.left = Node(2) 
     
     print('\n# #################### two_sum_bst function #################### #\n')
+    s = set()
 
-    print(' k=4 => ',findTarget(tree.root, 4))
-    print('k=9 => ',findTarget(tree.root, 9))
-    print('k=5 => ',findTarget(tree.root, 5))
-    print('k=5 => ',findTarget(tree.root, 5))
-    print('k=13 => ',findTarget(tree.root, 13))
-    print('k=20 => ',findTarget(tree.root, 20))
+    print(' k=4 => ',tsB(tree.root, 4,s))
+    print('k=9 => ',tsB(tree.root, 9,s))
+    print('k=5 => ',tsB(tree.root, 5,s))
+    print('k=8 => ',tsB(tree.root, 8,s))
+    print('k=13 => ',tsB(tree.root, 13,s))
+    print('k=20 => ',tsB(tree.root, 20,s))
 
 
